78a8c7feed4e018b33f781551b824a050f4bbf25
DoctorWater <malckov.schura2015@gmail.com>
2025-04-11 03:28:15 +0300

    refactor: major refactor of Lab5

--- nginx/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Подстановка переменных окружения в шаблон конфигурации nginx
-envsubst '$FRONTEND_PORT $BACKEND_PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-# Запуск nginx в режиме foreground
-exec nginx -g 'daemon off;'
